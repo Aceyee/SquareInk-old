bca11deb99bd8b805d35bd6d2d18c83d7c073255
Zihan Ye <zaeye1028@gmail.com>
2017-08-26 18:28:32 -0700

    icon designed and added

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 ﻿<?php
-     $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 4); //只取前4位，这样只判断最优先的语言。如果取前5位，可能出现en,zh的情况，影响判断。  
+     $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 4);
     if (preg_match("/zh-c/i", $lang)){
-        echo "简体中文";
+        require("./zh/nav.htm"); 
+        require("./zh/body.htm"); 
     } 
     else if (preg_match("/en/i", $lang)){   
-        echo "English";  
         require("./en/nav.htm"); 
         require("./en/body.htm"); 
     }  
